32708c91b5e78c721db548e2d4c0018b2348ec43
Vuks69 <51289041+Vuks69@users.noreply.github.com>
2022-11-07 18:39:23 +0100

    move tc commands to post-build

--- ips-server/server.sh
+++ ips-server/server.sh
@@ -1,6 +1,22 @@
 #!bin/sh
 set -xe -o ignoreeof
 
+# add qdisc
+tc qdisc add dev eth0 root handle 1: htb default 30
+
+# add root class
+tc class add dev eth0 parent 1: classid 1:1 htb rate 10mbps ceil 1mbps
+
+# add subclasses
+tc class add dev eth0 parent 1:1 classid 1:10 htb rate 1mbps ceil 6mbps
+tc class add dev eth0 parent 1:1 classid 1:20 htb rate 2mbps ceil 3mbps
+tc class add dev eth0 parent 1:1 classid 1:30 htb rate 100kbps ceil 1mbps
+
+# add filters that put packets in subclasses
+# cat /etc/protocols -> tcp = 6; udp = 17
+tc filter add dev eth0 parent 1:0 protocol ip prio 1 u32 match ip protocol 6 0xff flowid 1:10
+tc filter add dev eth0 parent 1:0 protocol ip prio 1 u32 match ip protocol 17 0xff flowid 1:20
+
 for port in "$@"; do
     iperf3 --server --port "${port}" --daemon --json --logfile /results/server/iperf3-${port}.log
 done
